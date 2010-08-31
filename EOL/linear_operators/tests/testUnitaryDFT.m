d8a484795cb8f5d25cb0663cca5fc13a7d0ca9dc
Eli Osherovich <oeli@cs.technion.ac.il>
2010-08-31 18:21:54 +0300

    Added a unit test for the UnitaryDFT class.

---
+++ EOL/linear_operators/tests/testUnitaryDFT.m
@@ -0,0 +1,45 @@
+function testUnitaryDFT
+initTestSuite;
+
+
+% Test that the constructor accepts only one or two inputs
+function testNArgIn
+% try zero inputs
+f = @() UnitaryDFT_eo();
+assertExceptionThrown(f, 'EOL:UnitaryDFT:WrongArgNum');
+
+% try 3 inputs
+f = @() UnitaryDFT_eo(1, 2, 3);
+assertExceptionThrown(f, 'MATLAB:maxrhs');
+
+
+function testForward
+% Without zero-padding.
+in = rand(2,3) + 1i*rand(2,3);
+expectedOut = fftn(in)/sqrt(numel(in));
+A = UnitaryDFT_eo(size(in));
+assertElementsAlmostEqual(expectedOut(:), A*in);
+
+% With zero-padding
+in = rand(2,3) + 1i*rand(2,3);
+expectedOut = fftn(in, [4, 6])/sqrt(4*6);
+A = UnitaryDFT_eo(size(in), [4, 6]);
+assertElementsAlmostEqual(expectedOut(:), A*in);
+
+function testAdjoint
+% Without zero-padding.
+in1 = rand(2,3) + 1i*rand(2,3);
+in2 = rand(2,3) + 1i*rand(2,3);
+A = UnitaryDFT_eo(size(in1));
+res1 = in2(:)'*(A*in1);
+res2 = (A'*in2)'*in1(:);
+assertElementsAlmostEqual(res1, res2);
+
+% With zero-padding.
+in1 = rand(2,3) + 1i*rand(2,3);
+in2 = rand(4,8) + 1i*rand(4,8);
+A = UnitaryDFT_eo(size(in1), size(in2));
+res1 = in2(:)'*(A*in1);
+res2 = (A'*in2)'*in1(:);
+assertElementsAlmostEqual(res1, res2);
+
