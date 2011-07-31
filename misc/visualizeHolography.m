5ee5ac8b6f60ff48f8cdf032faec33ff7b60b9b2
Eli Osherovich <oeli@cs.technion.ac.il>
2011-07-31 21:01:23 +0300

    minor changes to visualizeHolography

--- misc/visualizeHolography.m
+++ misc/visualizeHolography.m
@@ -13,14 +13,14 @@ O = complex(0, 0);
 plot(O, 'ko', 'MarkerFaceColor', 'k');
 
 % Draw the reference beam
-plot([0; refBeam(idx)], 'k');
+plot([0; refBeam(idx)], '-.k');
 hold on;
-axis square;
+axis equal;
 
 % Draw the circles corresponding to the reference beam (black), the
 % measured intensity (red), and the unknown signal (green).
 t = (0:0.001:1)*2*pi;
-plot(abs(refBeam(idx))*cos(t), abs(refBeam(idx))*sin(t), 'k');
+%plot(abs(refBeam(idx))*cos(t), abs(refBeam(idx))*sin(t), 'k');
 plot(sqrt(holImg(idx))*cos(t), sqrt(holImg(idx))*sin(t), 'r');
 plot(real(refBeam(idx)) + ...
     xMag(idx)*cos(t), imag(refBeam(idx)) + xMag(idx)*sin(t), 'g');
@@ -32,8 +32,8 @@ plot(real(refBeam(idx)) + ...
 yLo = sqrt(holImg(idx))*exp(1i*phaseLo(idx));
 yHi = sqrt(holImg(idx))*exp(1i*phaseHi(idx));
 y = (yLo+yHi)/2;
+
 % plot segment
-line_dir = (yHi - yLo) / abs(yHi - yLo);
 hLo = plot([O;yLo], 'k');
 hHi = plot([O;yHi], 'm');
 legend([hLo, hHi], 'Low', 'High');
